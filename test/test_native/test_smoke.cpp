be57ef2bed509dc34a0387dfe7e211bc87cf587b
JimboMcRackem <jim.batiste@gmail.com>
2026-07-29 20:21:13 +0100

    feat: add LogicalState and OutputIntent domain types

--- test/test_native/test_smoke.cpp
+++ test/test_native/test_smoke.cpp
@@ -1,5 +1,7 @@
 #include <unity.h>
 #include "domain/color.h"
+#include "domain/logical_state.h"
+#include "domain/output_intent.h"
 
 void test_rgb_equality() {
   Rgb a{255, 80, 0};
@@ -9,11 +11,19 @@ void test_rgb_equality() {
   TEST_ASSERT_TRUE(a != c);
 }
 
+// Forward declarations for tests defined in test_types.cpp
+void test_hazards_requires_both_indicators();
+void test_brake_is_front_or_rear();
+void test_output_intent_defaults_dark();
+
 void setUp(void) {}
 void tearDown(void) {}
 
 int main(int, char**) {
   UNITY_BEGIN();
   RUN_TEST(test_rgb_equality);
+  RUN_TEST(test_hazards_requires_both_indicators);
+  RUN_TEST(test_brake_is_front_or_rear);
+  RUN_TEST(test_output_intent_defaults_dark);
   return UNITY_END();
 }
---
+++ test/test_native/test_types.cpp
@@ -0,0 +1,27 @@
+#include <unity.h>
+#include "domain/logical_state.h"
+#include "domain/output_intent.h"
+
+void test_hazards_requires_both_indicators() {
+  LogicalState s;
+  s.leftInd = true;
+  TEST_ASSERT_FALSE(s.hazards());
+  s.rightInd = true;
+  TEST_ASSERT_TRUE(s.hazards());
+}
+
+void test_brake_is_front_or_rear() {
+  LogicalState s;
+  TEST_ASSERT_FALSE(s.brake());
+  s.rearBrake = true;
+  TEST_ASSERT_TRUE(s.brake());
+  s.rearBrake = false;
+  s.frontBrake = true;
+  TEST_ASSERT_TRUE(s.brake());
+}
+
+void test_output_intent_defaults_dark() {
+  OutputIntent o;
+  TEST_ASSERT_TRUE((o.frontL == Rgb{0, 0, 0}));
+  TEST_ASSERT_EQUAL_UINT8(0, o.denali);
+}
